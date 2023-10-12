85c01603e2a1b056adabff161d6eb169d3176980
chour <chour@shoo>
2023-10-12 13:54:13 +0100

    First commit

---
+++ ChedlyChourouk_LSI3_DepAppReparties_TP1/src/ClientPackage/Client.java
@@ -0,0 +1,28 @@
+package ClientPackage;
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Client {
+    public static void main(String[] args) {
+        try {
+            System.out.println("je suis un client");
+            Socket s =new Socket("localhost",1234);
+            System.out.println("je suis connecté au serveur");
+            int nb;
+            System.out.println("donner un nombre ");
+            Scanner scanner =new Scanner(System.in);
+            nb =scanner.nextInt();
+
+            OutputStream os =s.getOutputStream();
+
+            InputStream is =s.getInputStream();
+            os.write(nb);
+            int rep = is.read();
+            System.out.println("le produit de "+nb+"*5="+rep);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
